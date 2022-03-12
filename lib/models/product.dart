f1fa71b09d0a0423114afe33010ac61a71538391
Ossyyrr <ossyyrr@hotmail.com>
2022-03-12 13:38:53 +0100

    wip fetch

---
+++ lib/models/product.dart
@@ -0,0 +1,36 @@
+// To parse this JSON data, do
+//
+//     final product = productFromJson(jsonString);
+
+import 'dart:convert';
+
+class Product {
+  Product({
+    required this.available,
+    required this.name,
+    this.picture,
+    required this.price,
+  });
+
+  bool available;
+  String name;
+  String? picture;
+  double price;
+
+  Product productFromJson(String str) => Product.fromJson(json.decode(str));
+  String productToJson(Product data) => json.encode(data.toJson());
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        available: json["available"],
+        name: json["name"],
+        picture: json["picture"],
+        price: json["price"].toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "available": available,
+        "name": name,
+        "picture": picture,
+        "price": price,
+      };
+}
